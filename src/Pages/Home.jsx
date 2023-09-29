0aef40a20029eeff2ecd3870ea3e8e84ab9794dd
Sangarukarthik <146332803+Sangarukarthik@users.noreply.github.com>
2023-09-29 19:02:04 +0530

    29-09 commit
    home screen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+ import Carousel from 'react-bootstrap/Carousel';
+
 
 function Home() {
   return (
-    <div></div>
+    <>
+      <div className='home'>
+        <h1 className='home-text'>Groceries Delivered in 90 Minute</h1>
+        <p className='home-para text-center'>Get your healthy foods & snacks delivered at your doorsteps all day everyday</p>
+        <form class="form-inline home-search d-flex text-center">
+          <input class="form-control home-input " type="search" placeholder="Search your products from here" aria-label="Search" />
+          <button class="btn btn-outline-success btn-lg home-btn d-flex" type="submit"><span className='px-2'><FiSearch /></span> Search</button>
+        </form>
+      </div>
+      {/* <div className='home-img'>
+        <img src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F902%2Foffer-5.png&w=640&q=75' alt='img' />
+        <img src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F903%2Foffer-4.png&w=640&q=75' alt='img' />
+        <img src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='img' />
+      </div> */}
+
+<Carousel>
+      <Carousel.Item className='d-flex'>
+      <img className='d-block w-25' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='First-slide'/>
+      <img className='d-block w-25' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='First-slide'/>
+      <img className='d-block w-25' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='First-slide'/>
+      <img className='d-block w-25' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='First-slide'/>
+      <img className='d-block w-25' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='First-slide'/>
+      <img className='d-block w-25' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F904%2Foffer-3.png&w=640&q=75' alt='First-slide'/>
+        
+        
+      </Carousel.Item>
+      
+      
+    </Carousel>
+
+
+    </>
   )
 }
 
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { LuApple } from 'react-icons/lu'
+
 
 function Navbar() {
     return (
-        <nav className='navbar navbar-expand-lg nav-bg fixed-top'>
+        <nav className='navbar navbar-expand-lg nav-bg fixed-top pt-4'>
             <div className='container-fluid'>
-                {/* <a className='navbar-brand logo' href='/'>Pick<span>Bazar</span></a> */}
-                <img className='logo me-auto' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F860%2FPickBazar.png&w=1920&q=75' />
+                <a href='/'><img className='logo me-auto' src='https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F860%2FPickBazar.png&w=1920&q=75' alt='img' /></a>
+
+                <select class="form-select form-select-lg text-center align-items-center justify-content-center " aria-label=".form-select-lg example">
+                    <option selected><LuApple />Grocery</option>
+                    <option value="1">Bakery</option>
+                    <option value="2">Makeup</option>
+                    <option value="3">Bags</option>
+                    <option value="4">Clothing</option>
+                    <option value="5">Furniture</option>
+                    <option value="6">Daily Needs</option>
+                    <option value="7">Books</option>
+                </select>
+
+
+
                 <div>
                     <ul className='navbar-nav'>
                         <li>
-                            <Link className='li-items' to='/shops'>Shops </Link>
+                            <Link className='li-items mb-4' to='/shops'>Shops </Link>
                         </li>
                         <li>
                             <Link className='li-items' to='/offers'>Offers </Link>
@@ -21,8 +36,11 @@ function Navbar() {
                         <li>
                             <Link className='li-items' to='/contact'>Contact </Link>
                         </li>
-                        <a className='btn btn-primary btn-lg'>Become a Seller</a>
-                        <a className='btn btn-primary btn-lg'>Join</a>
+
+                        <form class="container-fluid d-flex justify-content-center align-items-center">
+                            <button class="btn btn-outline-success me-2" type="button">Become a Seller</button>
+                            <button class="btn btn-sm btn-outline-secondary" type="button">Join</button>
+                        </form>
                     </ul>
                 </div>
 
